d8c9d1ac64b57fc1748ba54703fba36dd35551ec
melfu <melular@gmail.com>
2019-01-31 19:07:20 -0600

    mel fuechec

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+const users = require("./state");
+const products = require("./state");
+
+app.get('/users', (request,response,next) => {
+    response.json(users)
+   });   
+app.get('/users/:userId', (request,response,next) => {
+    //request.params is built into express
+    const userId =  request.params.userId;
+    response.json(users[request.params.userId - 1])
+   });
+   app.post('/users', (request,response,next) => {
+       let user = request.body;
+       users.push(user);
+       response.json(user)
+   });   
+app.post('/users/:id', (request,response,next) => {
+    response.json({theidfromthepath:request.params.id})
+   });   
+
+app.put('/users/:id', (request,response,next) => {
+    response.json({theidfromthepath:request.params.id})
+   });    
+app.delete('/users/:id', (request,response,next) => {
+    response.json({theidfromthepath:request.params.id})
+   }); 
+
+app.listen(3002, (err) => {
+ if (err) {
+   return console.log("Error", err);
+ }
+ console.log("Web server is now listening for messages", err);
+});
 
